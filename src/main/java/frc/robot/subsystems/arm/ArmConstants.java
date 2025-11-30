bb32be17af817916cee4171696ae45b120aa6d03
yoavbillauer-stack <yoavbillauer@gmail.com>
2025-11-30 18:18:33 +0200

    done?

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -7,15 +7,17 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.measure.Angle;
 
 public class ArmConstants {
-    private static final int MOTOR_ID = 2;
-    private static final int ENCODER_ID = 1;
-
-    static final CANcoder ENCODER = new CANcoder(ENCODER_ID);
+    private static final int
+            MOTOR_ID =1,
+            ENCODER_ID = 1;
     static final TalonFX MOTOR = new TalonFX(MOTOR_ID);
+    static final CANcoder ENCODER = new CANcoder(ENCODER_ID);
+
     static final boolean FOC_ENABLED = true;
     static final PIDController PID_CONTROLLER = new PIDController(1,2,3);
     static final StatusSignal<Angle> ANGLE_ENCODER_POSITION_SIGNAL = ENCODER.getPosition();
@@ -23,11 +25,23 @@ public class ArmConstants {
     static {
         final CANcoderConfiguration cancoderConfig = new CANcoderConfiguration();
         final TalonFXConfiguration config = new TalonFXConfiguration();
+
         config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
         config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-        
-        MOTOR.getConfigurator().apply(config);
+        cancoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+
+        configureMotor();
+        configureEncoder();
         ANGLE_ENCODER_POSITION_SIGNAL.setUpdateFrequency(100);
         ENCODER.optimizeBusUtilization();
+        MOTOR.optimizeBusUtilization();
+    }
+    static void configureEncoder() {
+        final CANcoderConfiguration cancoderConfig = new CANcoderConfiguration();
+        ENCODER.getConfigurator().apply(cancoderConfig);
+    }
+    static void configureMotor() {
+        final TalonFXConfiguration config = new TalonFXConfiguration();
+        MOTOR.getConfigurator().apply(config);
     }
 }
