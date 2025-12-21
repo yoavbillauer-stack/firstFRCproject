496d225dc72327aca09c4c026ede180ca7775dda
yoavbillauer-stack <yoavbillauer@gmail.com>
2025-12-21 15:34:16 +0200

    tank.v.1
    אין דבר שכדור רטלין לא יכול לפתור בשעתיים

--- src/main/java/frc/robot/subsystem/tank/Tank.java
+++ src/main/java/frc/robot/subsystem/tank/Tank.java
@@ -1,4 +1,23 @@
 package frc.robot.subsystem.tank;
 
 public class Tank {
+    private void arcadeDrive() {
+        double forward = TankConstants.getForward();
+        double rotation = TankConstants.getRotation();
+
+        double leftPower = forward + rotation;
+        double rightPower = forward - rotation;
+
+        leftPower = checkIfValFits(leftPower, -TankConstants.MAX_SPEED, TankConstants.MAX_SPEED);
+        rightPower = checkIfValFits(rightPower, -TankConstants.MAX_SPEED, TankConstants.MAX_SPEED);
+
+        TankConstants.LEFT_FRONT_MOTOR.set(leftPower);
+        TankConstants.LEFT_REAR_MOTOR.set(leftPower);
+        TankConstants.RIGHT_FRONT_MOTOR.set(rightPower);
+        TankConstants.RIGHT_REAR_MOTOR.set(rightPower);
+    }
+
+    private double checkIfValFits(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
+    }
 }
--- src/main/java/frc/robot/subsystem/tank/TankConstans.java
+++
@@ -1,4 +0,0 @@
-package frc.robot.subsystem.tank;
-
-public class TankConstans {
-}
